45b1e3e61a0e445a3270fd259e36b759db79abb8
janakkhadka <janakkhadka08@gmail.com>
2024-12-31 14:59:53 +0545

    web

---
+++ web/web-frontend/src/components/UserDashboard/BloodAvailability.jsx
@@ -0,0 +1,109 @@
+import React,{useState} from 'react'
+
+import './BloodAvailability.css'
+
+import Select from 'react-select';
+import customStyles from '../LoginRegistration/ReactSelectStyle';
+
+import 'react-calendar/dist/Calendar.css';
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import '../LoginRegistration/Calender.css';
+
+import {provinceList, provinceHospitalList, hospitalBloodDataList} from '../LoginRegistration/DropDownList';
+
+import { IoIosArrowDropdownCircle } from "react-icons/io";
+
+import BackThreeD from '../LoginRegistration/3d'
+import UserNavigationBar from './UserNavigationBar'
+import MyBarChart from '../Utils/MyBarChart';
+
+
+function BloodAvailability() {
+    const [hospitalOptions, setHospitalOptions] = useState([]);//list of hospital accordance to province hai
+    const [selectedProvince, setSelectedProvince] = useState('')
+    const handleProvinceChange = (selectedOption) => {
+        setSelectedProvince(selectedOption);
+        setSelectedHospital(null);
+    
+        const selectedProvinceData = provinceHospitalList.find(
+          (province) => province.label === selectedOption.label
+        );
+    
+        const updatedHospitalOptions = selectedProvinceData
+          ? selectedProvinceData.options
+          : [];
+    
+        setHospitalOptions(updatedHospitalOptions); // Update the district options
+      };
+
+    const [hospitalBloodDataOptions, setHospitalBloodDataOptions] = useState([]); //hospital blood data ko lagi, hospital choose garesi tesko data halna lai
+
+    const [selectedHospital, setSelectedHospital] = useState("")
+    const handleHospitalChange = (selectedOption) => {
+        setSelectedHospital(selectedOption);
+        // setSelectedHospital(null);
+    
+        const selectedHospitalData = hospitalBloodDataList.find(
+          (hospital) => hospital.name === selectedOption.label
+        );
+    
+        const updatedHospitalBloodDataOptions = selectedHospitalData
+          ? selectedHospitalData.bloodType
+          : [];
+    
+        setHospitalBloodDataOptions(updatedHospitalBloodDataOptions); // Update the district options
+      };
+
+    
+
+
+  return (
+    <div className='blood-availability-wrapper'>
+        <div className="syringe">
+            <BackThreeD/>
+        </div>
+        <UserNavigationBar titleBar="Blood Availability"/>
+        <div className="blood-availability">
+            <form action="">
+                <h1>See Blood Availability</h1>
+                <span>Please choose the Hospital to see Blood Availability.</span>
+
+                <div className="drop-down-box" style={{marginTop:"18px"}}>
+                    <Select
+                        value = {selectedProvince}
+                        onChange={handleProvinceChange}
+                        options={provinceList}
+                        styles={customStyles}
+                        placeholder="Koshi Province"
+                        isSearchable={false}
+                    />
+                    <IoIosArrowDropdownCircle className='icon'/>
+                </div>
+
+                <div className="drop-down-box">
+                    <Select
+                        value = {selectedHospital}
+                        onChange={handleHospitalChange}
+                        options={hospitalOptions}
+                        styles={customStyles}
+                        placeholder={
+                        selectedProvince ? "Select a Hospital" : "BPKIHS Dharan"
+                        }
+                        isDisabled={!selectedProvince}
+                        isSearchable={false}
+                    />
+                    <IoIosArrowDropdownCircle className='icon'/>
+                </div>
+            </form>
+            <div className="bar-chart">
+                <MyBarChart barList ={hospitalBloodDataOptions}/>
+            </div>
+            
+        </div>
+        
+    </div>
+  )
+}
+
+export default BloodAvailability
---
+++ web/web-frontend/src/components/UserDashboard/UserDashboardHome.jsx
@@ -0,0 +1,158 @@
+import React from 'react'
+
+import './UserDashboardHome.css'
+
+import { format } from "date-fns";
+import { useNavigate } from 'react-router-dom';
+
+import { MdOutlineDateRange }from "react-icons/md";
+import { IoMdTime } from "react-icons/io";
+import { IoLocationOutline } from "react-icons/io5";
+import { IoMdOptions } from "react-icons/io";
+
+import { getGreeting } from '../Utils/Greeting'
+import {events, urgentBlood} from './DummyData'
+
+function UserDashboardHome() {
+  const navigate = useNavigate();
+  return (
+    <div className='user-dashboard-home-wrapper'>
+      <div className="user-dashboard-top-section">
+        <div className="left-section">
+          <section className="user-greeting-message">
+                <h1>{getGreeting()+", Janak Khadka!"}</h1>
+                <div className="user-typing-effect">
+                  <span>Happy to see you.</span>
+                </div>
+          </section>
+          <section className="eligibility-message">
+            <h1>Donation Eligibility</h1>
+            <span>You are eligible to donate</span>
+          </section>
+          <section className='quick-actions'>
+            <h1>Fast Actions!</h1>
+            <button className="action-button" onClick={() => navigate("/blood-request-form")}>Request Blood Now</button>
+            <button className="action-button" onClick={() => navigate("/schedule-donation")}>Schedule a donation</button>
+            <button className="action-button" onClick={() => navigate("/user-organize-event")}>Organize an event</button>
+            <button className="action-button" onClick={() => navigate("/user-blood-availability")}>See Blood Availability</button>
+            <button className="action-button" onClick={() => navigate("/user-profile")}>Visit Your Profile</button>
+          </section>
+        </div>
+        <div className="right-section">
+
+          <section className='user-reminder-wrapper'>
+            <h1>Quick Reminder!</h1>
+              <div className="user-reminder">
+                <div className="next-scheduled-donation">
+                  <h3>Next Scheduled Donation</h3>
+                  <div className="reminder-info">
+                    <div className="icon-info-wrapper">
+                      <MdOutlineDateRange/>
+                      <span>Scheduled Date: <span style={{fontWeight:"Bold"}}>December 24, 2024</span></span>
+                    </div>
+                    <div className="icon-info-wrapper">
+                      <IoLocationOutline/>
+                      <span>Venue: <span style={{fontWeight:"Bold"}}>BPKHIS Dharan</span></span>
+                    </div>
+                    <div className="icon-info-wrapper">
+                      <IoMdTime/>
+                      <span>Time: <span style={{fontWeight:"Bold"}}>12:00pm-3:00pm</span></span>
+                    </div>
+                  </div>
+                </div>
+                <div className="nearest-interested-event">
+                  <h3>Upcoming Event</h3>
+                  <div className="reminder-info">
+                    <div className="icon-info-wrapper">
+                      <MdOutlineDateRange/>
+                      <span>Event Date: <span style={{fontWeight:"Bold"}}>December 24, 2024</span></span>
+                    </div>
+                    <div className="icon-info-wrapper">
+                      <IoLocationOutline/>
+                      <span>Venue: <span style={{fontWeight:"Bold"}}>BPKHIS Dharan</span></span>
+                    </div>
+                    <div className="icon-info-wrapper">
+                      <IoMdOptions/>
+                      <span>For: <span style={{fontWeight:"Bold"}}>Donating/Volunteering</span></span>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </section>
+          
+          <section className='urgent-blood'>
+            <div className="h1-urgent">
+              <h1>Urgent Blood Needed!</h1>
+            </div>
+            <table border="0" style={{tableLayout: "fixed", width: "100%", borderCollapse: "collapse" }}>
+              <colgroup>
+                <col style={{ width: "20%" }} />
+                <col style={{ width: "40%" }} /> 
+                <col style={{ width: "17%" }} />
+                <col style={{ width: "23%" }} /> 
+              </colgroup>
+              <thead>
+                <tr>
+                  <th>Requested on</th>
+                  <th>Location</th>
+                  <th style={{textAlign:"center"}}>Blood Type</th>
+                  <th style={{textAlign:"center"}}>Wants to donate?</th>
+                </tr>
+              </thead>
+              <tbody>
+                {urgentBlood.map((urgentB, index) => (
+                  <tr key={index}>
+                    <td>{format(urgentB.date, "MMMM dd, yyyy")}</td> {/* Format date */}
+                    <td  className='table-data'>{urgentB.location}</td>
+                    <td style={{textAlign: 'center'}}>{urgentB.bloodType}</td>
+                    <td style={{textAlign:"center"}}><button className="notify-button" onClick={() => navigate("/")}>Notify</button></td>
+                    
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </section>
+        </div>
+      </div>
+      <div className="user-dashboard-bottom-section">
+        <section className='donation-events'>
+            <div className="h1-events">
+              <h1>Upcoming Blood Donation Events</h1>
+            </div>
+            <table border="0" style={{tableLayout: "fixed", width: "100%", borderCollapse: "collapse" }}>
+              <colgroup>
+                <col style={{ width: "15%" }} /> 
+                <col style={{ width: "27%" }} /> 
+                <col style={{ width: "20%" }} /> 
+                <col style={{ width: "20%" }} /> 
+                <col style={{ width: "17%" }} /> 
+              </colgroup>
+              <thead>
+                <tr>
+                  <th>Date</th>
+                  <th>Event</th>
+                  <th>Location</th>
+                  <th style={{textAlign:"center"}}>Wants to volunteering?</th>
+                  <th style={{textAlign:"center"}}>Wants to donate?</th>
+                </tr>
+              </thead>
+              <tbody>
+                {events.map((event, index) => (
+                  <tr key={index}>
+                    <td>{format(event.date, "MMMM dd, yyyy")}</td> {/* Format date */}
+                    <td className='table-data'>{event.title}</td>
+                    <td className='table-data'>{event.location}</td>
+                    <td style={{textAlign:"center"}}><button className="notify-button" onClick={() => navigate("/")}>Notify</button></td>
+                    <td style={{textAlign:"center"}}><button className="notify-button" onClick={() => navigate("/")}>Notify</button></td>
+                    
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </section>
+      </div>
+    </div>
+  )
+}
+
+export default UserDashboardHome
