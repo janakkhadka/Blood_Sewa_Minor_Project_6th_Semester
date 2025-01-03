e3a14a2f146fe3e1c2f2fb8b1ae1491099e9d810
kiran <kiran.211720@ncit.edu.np>
2025-01-03 19:40:09 +0545

    donation profile and info added

--- mobile/Donation/app/src/main/java/com/example/donation/BottomNavBar/ProfileScreen.kt
+++ mobile/Donation/app/src/main/java/com/example/donation/BottomNavBar/ProfileScreen.kt
@@ -6,28 +6,41 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Badge
+import androidx.compose.material.icons.filled.Bloodtype
+import androidx.compose.material.icons.filled.Cached
 import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material.icons.filled.Countertops
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.LocalFlorist
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.PersonOutline
 import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.RemoveRedEye
 import androidx.compose.material.icons.filled.Transgender
 import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material.icons.rounded.SupervisedUserCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -48,6 +61,7 @@ import com.example.donation.ui.theme.dRed
 
 @Composable
 fun ProfileScreen(navController : NavHostController) {
+    val scroll   = rememberScrollState()
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.TopCenter
@@ -64,6 +78,7 @@ fun ProfileScreen(navController : NavHostController) {
                     .padding(top = 10.dp)
                     .align(Alignment.CenterHorizontally)
                     .shadow(elevation = 50.dp)
+                    .verticalScroll(scroll)
                     .clip(shape = RoundedCornerShape(10.dp))
                     .background(Color.White),
                 contentAlignment = Alignment.TopStart
@@ -90,10 +105,10 @@ fun ProfileScreen(navController : NavHostController) {
                        }
                    }
 
-                    Details(Icons.Default.CalendarMonth,"Date of Birth","2003")
-                    Details(Icons.Default.CalendarMonth,"Gender","Male")
-                    Details(Icons.Default.Phone,"Phone","+977-98675445343")
-                    Details(Icons.Default.Home,"Address","Kalikot,Karnali Province")
+                    PDetails(Icons.Default.CalendarMonth,"Date of Birth:","2003")
+                    PDetails(Icons.Default.CalendarMonth,"Gender:","Male")
+                    PDetails(Icons.Default.Phone,"Phone:","+977-98675445343")
+                    PDetails(Icons.Default.Home,"Address:","Kalikot,Karnali Province")
 
                     Box(
                         modifier = Modifier.fillMaxWidth()
@@ -112,6 +127,89 @@ fun ProfileScreen(navController : NavHostController) {
                     }
                 }
             }
+            //donation details
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(.95f)
+                    .padding(top = 10.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .shadow(elevation = 50.dp)
+                    .clip(shape = RoundedCornerShape(10.dp))
+                    .background(Color.White),
+                contentAlignment = Alignment.TopStart,
+
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(10.dp),
+                    modifier = Modifier.padding(start = 10.dp,top = 10.dp)
+
+
+
+                    ) {
+                    Text(text = "Donation Profile", fontWeight = FontWeight.Bold, color = dRed, fontSize = 22.sp)
+                    Text(text = "Your donation have made a difference!", fontSize = 14.sp)
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.spacedBy(10.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Column() {
+                            Details(Icons.Default.LocalFlorist, "Current Badge:", "Heroic")
+                            Row(
+                                modifier = Modifier.padding(start = 10.dp, top = 10.dp),
+
+                                ) {
+                                Text(text = " Progress to next milestone")
+                                Text(text = "25%", fontWeight = FontWeight.Bold)
+
+                            }
+                            Spacer(modifier = Modifier.height(10.dp))
+                            Details(Icons.Default.Bloodtype, "Blood Type", "AB-")
+                        }
+                        Image(
+                            Icons.Default.Cached,
+                            contentDescription = "",
+                            modifier = Modifier.size(100.dp)
+                        )
+                    }
+
+                }
+            }
+            //donation information
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(.95f)
+                    .padding(top = 10.dp)
+                    .align(Alignment.CenterHorizontally)
+                    .shadow(elevation = 50.dp)
+                    .clip(shape = RoundedCornerShape(10.dp))
+                    .background(Color.White),
+                contentAlignment = Alignment.TopStart,
+
+                ) {
+
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(10.dp),
+                        modifier = Modifier.padding(start = 10.dp, top = 10.dp)
+
+
+                    ) {
+                        Text(text = " Donation Information", fontWeight = FontWeight.Bold, color = dRed, fontSize = 22.sp)
+
+                        Details(Icons.Default.Countertops, "Donation Count:", "21")
+                        Details(Icons.Default.CalendarMonth, "Last Donation Date:", "December 1, 2024")
+                        Details(Icons.Default.LocationOn, "Last Donation Event:", "KMC Hospital")
+
+                        Text(
+                            text = " Your are currently elligible to Donate Blood",
+                            modifier = Modifier.padding(bottom = 10.dp),
+                            color = dRed
+                        )
+
+
+
+                }
+            }
         }
 
     }
@@ -125,14 +223,15 @@ fun Details(
     Icons : ImageVector,
     text : String,
     value : String
-){
+) {
     Row(
         modifier = Modifier.padding(start = 10.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
             Icons,
-            contentDescription = ""
+            contentDescription = "",
+            colorFilter = ColorFilter.tint(dRed)
         )
         Row(
             modifier = Modifier.padding(start = 10.dp),
@@ -140,14 +239,42 @@ fun Details(
             verticalAlignment = Alignment.CenterVertically
 
         ) {
-            Text(text = "$text:")
-            Text(text = value)
+            Text(text = text, color = dRed)
+            Text(text = value, color = dRed, fontWeight = FontWeight.Bold)
 
         }
     }
-
-
 }
+
+    @Composable
+    fun PDetails(
+        Icons : ImageVector,
+        text : String,
+        value : String
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 10.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                Icons,
+                contentDescription = ""
+            )
+            Row(
+                modifier = Modifier.padding(start = 10.dp),
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                verticalAlignment = Alignment.CenterVertically
+
+            ) {
+                Text(text = text)
+                Text(text = value)
+
+            }
+        }
+
+
+    }
+
 
 
 @Preview(showBackground = true)
@@ -156,3 +283,5 @@ fun show(){
     val navController = rememberNavController()
     ProfileScreen(navController = navController)
 }
+
+
