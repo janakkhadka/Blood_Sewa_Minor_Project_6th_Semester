f29d6c3e945bec51ee2e65e0695179efe24f3e14
janakkhadka <janakkhadka08@gmail.com>
2025-01-04 08:50:22 +0545

    organization dashboard start

---
+++ web/web-frontend/src/components/OrgDashboard/OrgDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './OrgDashboard.css'
+
+function OrgDashboard() {
+  return (
+    <div>OrgDashboard</div>
+  )
+}
+
+export default OrgDashboard
--- web/web-frontend/src/components/Show.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Show = ({isVisible,children}) => {
-  return isVisible? children: null
-}
-
-export default Show
