9cb53428d00305cfc02a32034161612eb5f628a8
janakkhadka <janakkhadka08@gmail.com>
2025-01-04 17:38:49 +0545

    blink effect for alert added

--- web/web-frontend/src/components/OrgDashboard/OrgDashboard.jsx
+++ web/web-frontend/src/components/OrgDashboard/OrgDashboard.jsx
@@ -30,15 +30,20 @@ function OrgDashboard() {
                 <section className='quick-actions'>
                 <h1>Quick Actions!</h1>
                     <button className="action-button" onClick={() => navigate("/blood-request-form")}>Urgent Blood Request</button>
-                    <button className="action-button" onClick={() => navigate("/schedule-donation")}>Schedule a donation</button>
-                    <button className="action-button" onClick={() => navigate("/user-organize-event")}>Organize an event</button>
+                    <button className="action-button" onClick={() => navigate("/schedule-donation")}>Scheduled Donation</button>
+                    <button className="action-button" onClick={() => navigate("/user-organize-event")}>Organize an Event</button>
                     <button className="action-button" onClick={() => navigate("/user-blood-availability")}>Blood Inventory</button>
                     <button className="action-button" onClick={() => navigate("/search-donor")}>Find Donor</button>
                 </section>
             </div>
             <div className="right-section">
                 <div className="right-top-section">
-                    <h1>Blood Inventory</h1>
+                    <div className="h1-wrapper">
+                        <h1>Blood Inventory</h1>
+                        <div className="alert-wrapper">
+                            <h3 className='alert'>Alert!</h3>
+                        </div>
+                    </div>
                     <div className="bar-chart-wrapper">
                         <div className="chart-info-wrapper">
                             <div className="chart-info">
