dd4dee9f997b07db77cadf0cf8dde32469ab61fd
janakkhadka <janakkhadka08@gmail.com>
2025-01-20 17:18:56 +0545

    adding three state toggle

---
+++ web/web-frontend/src/components/Common/ToggleTheme.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./ToggleTheme.css";
+
+export default function ToggleTheme() {
+  const [theme, setTheme] = useState("light"); // Initial state set to "light"
+
+  const toggleTheme = () => {
+    setTheme((currentTheme) => {
+      switch (currentTheme) {
+        case "light":
+          return "dark";
+        case "dark":
+          return "custom";
+        case "custom":
+          return "light";
+        default:
+          return "light";
+      }
+    });
+  };
+
+  return (
+    <button onClick={toggleTheme} className={`toggle-button ${theme}`}>
+      <span className="sr-only">Toggle theme</span>
+      <span className="icon light-icon">☀️</span>
+      <span className="icon dark-icon">🌙</span>
+      <span className="icon custom-icon">🎨</span>
+      <span className="slider"></span>
+    </button>
+  );
+}
