94c6727b57e48371a0630a44205d6b8449631eb5
janakkhadka <janakkhadka08@gmail.com>
2025-02-10 20:53:28 +0545

    user navbar name show complete

--- web/web-frontend/src/components/UserDashboard/UserNavbarComponent.jsx
+++ web/web-frontend/src/components/UserDashboard/UserNavbarComponent.jsx
@@ -97,9 +97,20 @@ export const UserDashboardNavbarRightRight = () => {
         state: { accountType: "user" }
       });   
     };
+    const userDetailsString = sessionStorage.getItem('userDetails') || localStorage.getItem('userDetails');
+    let userDetails = null;
+    let userName = "";
+    
+    try {
+      userDetails = userDetailsString ? JSON.parse(userDetailsString) : null;
+      console.log(userDetails);
+      userName = userDetails.name;
+    } catch (error) {
+      console.error('Failed to parse userDetails:', error);
+    }
     return (
       <div className="navbar-right-right">
-        <span>Welcome, Janak</span>
+        <span>Welcome, {userName}</span>
         <button className="logout-button" onClick={handleLogout}>
           Logout
         </button>
