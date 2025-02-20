f0fd3dadeb092ae05208c3365ccae228289b1416
Janak Khadka <janakkhadka08@gmail.com>
2025-02-20 12:02:52 +0545

    collab request manage start

--- web/web-frontend/src/Logic/API.jsx
+++ web/web-frontend/src/Logic/API.jsx
@@ -1,5 +1,5 @@
-export const api = 'http://192.168.23.97:8000/api/'
-export const localhost = 'http://192.168.23.97:8000'
+export const api = 'http://192.168.18.120:8000/api/'
+export const localhost = 'http://192.168.18.120:8000'
 
 
 // export const api = 'http://127.0.0.1:8000/api/'
