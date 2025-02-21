0c4400dbe375528ebb71223e1341071550572a66
BishalParajulii <parajulibishal00@gmail.com>
2025-02-21 19:39:50 +0545

    added notification for event creation

--- backend/project/app/consumers.py
+++ backend/project/app/consumers.py
@@ -25,3 +25,29 @@ class NotificationConsumer(AsyncWebsocketConsumer):
         """Send notification to all WebSocket clients"""
         message = event["message"]
         await self.send(text_data=json.dumps({"message": message}))
+
+
+
+
+class EventNotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_group_name = "events"
+        
+        # Add WebSocket to group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+        print("WebSocket Connected!")
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+        print("WebSocket Disconnected!")
+
+    async def send_event_notification(self, event):
+        message = event["message"]
+        await self.send(text_data=json.dumps({"event_notification": message}))
--- backend/project/app/routing.py
+++ backend/project/app/routing.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
-from .consumers import NotificationConsumer
+from .consumers import NotificationConsumer , EventNotificationConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/notifications/$', NotificationConsumer.as_asgi()),
+    re_path(r"ws/event-notification/$", EventNotificationConsumer.as_asgi()),  
 ]
